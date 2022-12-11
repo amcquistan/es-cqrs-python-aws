fbd971f03224132188a8fa03870cc3bb904698b0
Adam McQuistan <sci.guy.mcq@gmail.com>
2022-12-11 09:48:52 -0600

    Initial commit

---
+++ tests/unit/test_availability_aws_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from availability_aws.availability_aws_stack import AvailabilityAwsStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in availability_aws/availability_aws_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = AvailabilityAwsStack(app, "availability-aws")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
