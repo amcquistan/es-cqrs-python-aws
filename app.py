46ac62728deced6e6f37cda263477ecb585b5690
Adam McQuistan <sci.guy.mcq@gmail.com>
2022-12-14 22:08:18 -0600

    wip

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 
 import aws_cdk as cdk
 
-from availability_aws.infra_stack import InfraStack
+from aws.infra_stack import InfraStack
 
 
 app = cdk.App()
--- tests/unit/test_availability_aws_stack.py
+++ tests/unit/test_availability_aws_stack.py
@@ -1,7 +1,7 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
 
-from availability_aws.availability_aws_stack import AvailabilityAwsStack
+from aws.availability_aws_stack import AvailabilityAwsStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in availability_aws/availability_aws_stack.py
